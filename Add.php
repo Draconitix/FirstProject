99866cfd466df0c34ea036244339fbdc85fba5b4
Greesh12 <134640893+Greesh12@users.noreply.github.com>
2023-05-31 22:40:53 -0400

    Add files via upload

---
+++ Add.php
@@ -0,0 +1,44 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$Phone = $inData["Phone"];
+	$Email = $inData["Email"];
+	$Address = $inData["Address"];
+	$UserID = $inData["UserID"];
+
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error)
+	{
+			returnWithError( $conn->connect_error );
+	}
+	else
+	{
+			$stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Phone, Email, Address, UserID) VALUES(?,?,?,?,?,?)");
+			$stmt->bind_param("sssssi", $FirstName, $LastName, $Phone, $Email, $Address, $UserID);
+			$stmt->execute();
+			$stmt->close();
+			$conn->close();
+			returnWithError("");
+		}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
---
+++ Update.php
@@ -0,0 +1,48 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$newFirstName = $inData["newFirstName"];
+	$newLastName = $inData["newLastName"];
+	$Email = $inData["Email"];
+	$Phone = $inData["Phone"];
+	$newAddress = $inData["newAddress"];
+	$ID = $inData["ID"];
+
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+		if ($conn->connect_error)
+		{
+			returnWithError( $conn->connect_error );
+		}
+		else
+		{
+			$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Email = ?, Phone = ?, Address = ? WHERE ID = ?");
+			$stmt->bind_param("sssssi", $newFirstName, $newLastName,$Email,$Phone,$newAddress,$ID);
+			$stmt->execute();
+
+			$stmt->close();
+			$conn->close();
+			returnWithError("");
+		}
+
+
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
