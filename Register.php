fa3824d7299e0c508db94e9499fa202039077591
JohnnyB0132 <113563008+JohnnyB0132@users.noreply.github.com>
2023-06-02 07:19:52 -0400

    Add files via upload

---
+++ Register.php
@@ -0,0 +1,63 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$FirstName = $inData["FirstName"];
+    $LastName = $inData["LastName"];
+    $Username = $inData["Username"];
+    $Password = $inData["Password"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$sql = "SELECT * FROM Users WHERE Username=?";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("s", $Username);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$rows = mysqli_num_rows($result);
+		if ($rows == 0)
+		{
+			$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Username, Password) VALUES(?,?,?,?)");
+			$stmt->bind_param("ssss", $FirstName, $LastName, $Username, $Password);
+			$stmt->execute();
+			$id = $conn->insert_id;
+			$stmt->close();
+			$conn->close();
+			http_response_code(200);
+			$searchResults .= '{'.'"id": "'.$id.''.'"}';
+
+			returnWithInfo($searchResults);
+		} else {
+			http_response_code(409);
+			returnWithError("Username taken");
+		}
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
